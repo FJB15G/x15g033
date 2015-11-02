5578e2c340e47df7b50dd11c61dbd2abd33cc5dd
unknown <x15g033@PC504N015.chiba-fjb.ac.jp>
2015-11-02 14:40:28 +0900

    課題102

---
+++ src/jp/ac/chibafjb/x15g033/kd102/Sin102.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g033.kd102;
+
+public class Sin102 {
+	public static void main(String[] args) {
+		System.out.println("01 渡邊大地");
+		System.out.println("02 渡邊大地");
+		System.out.println("03 渡邊大地");
+		System.out.println("04 渡邊大地");
+		System.out.println("05 渡邊大地");
+		System.out.println("06 渡邊大地");
+		System.out.println("07 渡邊大地");
+		System.out.println("08 渡邊大地");
+		System.out.println("09 渡邊大地");
+		System.out.println("10 渡邊大地");
+	
+	}
+}
