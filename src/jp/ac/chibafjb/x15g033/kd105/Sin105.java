3c150796b22a60c6973e34da0e0edb41158ecffa
unknown <x15g033@PC504N015.chiba-fjb.ac.jp>
2015-11-02 14:55:36 +0900

    課題105

---
+++ src/jp/ac/chibafjb/x15g033/kd105/Sin105.java
@@ -0,0 +1,9 @@
+package jp.ac.chibafjb.x15g033.kd105;
+
+public class Sin105 {
+	public static void main(String[] args) {
+	System.out.println("氏名　： 渡邊　大地（わたなべ　だいち）");
+	System.out.println("血液型：???");
+	System.out.println("趣味　：???");
+	
+}}
