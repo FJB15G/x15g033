9cbe410a43f7ae7db6da94b66aff38d8aed39af4
unknown <x15g033@PC504N015.chiba-fjb.ac.jp>
2015-11-02 14:25:28 +0900

    課題101

---
+++ src/jp/ac/chibafjb/x15g033/kd101/Sin101.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g033.kd101;
+
+public class Sin101 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("船橋情報ビジネス専門学校");
+System.out.println("047-425-1051");	
+System.out.println("Heuuo☆こんにちは");
+	}
+
+}
