5607d1acd5b9c82c1d72472dd6cf13e0dd5520f0
unknown <x15g033@PC504N015.chiba-fjb.ac.jp>
2015-11-02 14:54:30 +0900

    課題１０３

---
+++ src/jp/ac/chibafjb/x15g033/kd103/Sin103.java
@@ -0,0 +1,8 @@
+package jp.ac.chibafjb.x15g033.kd103;
+
+public class Sin103 {
+	public static void main(String[] args) {
+		System.out.println("01 渡邊大地 02 渡邊大地 03 渡邊大地 04 渡邊大地 05 渡邊大地 06 渡邊大地 07 渡邊大地 08 渡邊大地 09 渡邊大地 10 渡邊大地");
+	}
+
+}
