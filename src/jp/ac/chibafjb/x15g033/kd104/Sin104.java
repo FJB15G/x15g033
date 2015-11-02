bcec0d950b76f2b89277e012ba02d5e7762d92b5
unknown <x15g033@PC504N015.chiba-fjb.ac.jp>
2015-11-02 14:55:06 +0900

    課題104

---
+++ src/jp/ac/chibafjb/x15g033/kd104/Sin104.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g033.kd104;
+
+public class Sin104 {
+	public static void main(String[] args) {
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");		
+	}
+
+}
