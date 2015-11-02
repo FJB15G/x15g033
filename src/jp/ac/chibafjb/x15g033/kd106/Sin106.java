27e541369c740cc491e81fd3076d9a2c977b5904
unknown <x15g033@PC504N015.chiba-fjb.ac.jp>
2015-11-02 14:58:29 +0900

    課題106

--- src/jp/ac/chibafjb/x15g033/kd106/Sin106.java
+++ src/jp/ac/chibafjb/x15g033/kd106/Sin106.java
@@ -4,7 +4,27 @@ public class Sin106 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		 System.out.println("	   a@@@a a@@@a           ");
+		  System.out.println("	  @@@@@@e@@@@@@          ");
+		  System.out.println("	 (@@@@@@@@@@@@@)         ");
+		  System.out.println("	  @@@@@@@@@@@@@          ");
+		  System.out.println("	   @@@@@@@@@@@           ");
+		  System.out.println("	    ~@@@@@@@~            ");
+		  System.out.println("	      ~@@@~              ");
+		  System.out.println("	       ~@~　　　ハート！ ");
+
+
+
+		  System.out.println("　        ＋―――――――――――＋ ");
+		  System.out.println("　　　　　│　　／￣￣￣＼　　＿　│ ");
+		  System.out.println("　《《　　│　／o）《《《 ＼／ ／ │ ");
+		  System.out.println("　  》》　│ │――――――   く　 │ ");
+		  System.out.println("　┌―┐　│　＼o）《《《 ／＼ ＼ │ ");
+		  System.out.println("　│　│　│　　＼＿＿＿／ △ ￣　│ ");
+		  System.out.println("　└―┘　＋―――――――――――＋ ");
+		  System.out.println("            　あじのひらき            ");
 
 	}
+	}
 
-}
+
